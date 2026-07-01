7138cda2a0f25d74371e7315a247a662e43d636f
大島大河 <oshima.automate.growth@gmail.com>
2026-07-01 10:43:32 +0900

    update:カスケードでスタッフテーブルも消えるか検証

--- src/backend/application/usecase/DeleteUserUseCase.spec.ts
+++ src/backend/application/usecase/DeleteUserUseCase.spec.ts
@@ -9,9 +9,15 @@ import {
     NodeUtilities,
 } from '@mydx-dev/gas-boost-runtime/testing';
 import { describe, expect, it, vi } from 'vitest';
+import { zocker } from 'zocker';
+import { context } from '../../../../tests/contexts/deleteUserTestContext';
 import { Role } from '../../../shared/domain/entity/Role';
 import { User } from '../../../shared/domain/entity/User';
-import { ALL_TABLES } from '../../infrastructure/database/tables';
+import {
+    ALL_TABLES,
+    EmployeeTable,
+    UserTable,
+} from '../../infrastructure/database/tables';
 import { DeleteUserUseCase } from './DeleteUserUseCase';
 
 function factory() {
@@ -108,25 +114,53 @@ describe('認可', () => {
 
 describe('シーケンス', () => {
     it('ユーザー削除を呼び出す', () => {
-        const { usecase } = factory();
-        const { deleteSpy, findSpy } = getDependencies(usecase);
-        const adminUserId = '123e4567-e89b-42d3-a456-426614174000';
-        const adminUser = new User(
-            adminUserId,
-            'Test User',
-            'admin@example.com',
-            'ValidPass123',
-            true,
-            1
+        console.log(
+            UserTable.getRelationTree().map((r) => ({
+                child: r.childTable.name,
+                onDelete: r.onDelete,
+            }))
         );
-        adminUser.addRelation(Role, new Role(adminUserId, 'システム管理者'));
+        const {
+            deleteUser: { usecase },
+            dbSpy: { deleteSpy, findSpy },
+            dataStore,
+        } = context();
+        const adminUserRecord = zocker(UserTable.schema)
+            .supply(UserTable.schema.shape.承認, true)
+            .generate();
+        const adminUser = UserTable.deserialize(adminUserRecord);
+
+        const deleteUserRecord = zocker(UserTable.schema)
+            .supply(UserTable.schema.shape.承認, true)
+            .generate();
+        const deleteUser = UserTable.deserialize(deleteUserRecord);
+
+        dataStore.set(`${UserTable.dbId}:${UserTable.name}`, [
+            Object.keys(UserTable.schema.def.shape),
+            Object.values(deleteUserRecord),
+        ]);
+
+        dataStore.set(`${EmployeeTable.dbId}:${EmployeeTable.name}`, [
+            Object.keys(EmployeeTable.schema.def.shape),
+            [deleteUser.id],
+        ]);
+
+        console.log(dataStore['storage']);
+        adminUser.addRelation(Role, new Role(adminUser.id, 'システム管理者'));
         findSpy.mockReturnValueOnce([adminUser]);
-        deleteSpy.mockReturnValue(true);
 
-        const deleteUserId = '123e4567-e89b-42d3-a456-426614174001';
-        usecase.execute(adminUserId, deleteUserId);
+        usecase.execute(adminUser.id, deleteUser.id);
 
-        expect(deleteSpy).toHaveBeenCalledWith([deleteUserId]);
+        expect(deleteSpy).toHaveBeenCalledWith([deleteUser.id]);
+
+        const remainingUsers = dataStore.get(
+            `${UserTable.dbId}:${UserTable.name}`
+        ).rows;
+        expect(remainingUsers).toEqual([]);
+        const remainingEmployees = dataStore.get(
+            `${EmployeeTable.dbId}:${EmployeeTable.name}`
+        ).rows;
+        expect(remainingEmployees).toEqual([]);
     });
 
     it('ユーザー削除に失敗した場合はエラーになる', () => {
